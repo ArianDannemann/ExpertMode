81916b9b0530660c6003348bf9334d4b0f724683
Arian Dannemann <arian.dannemann@gmail.com>
2019-07-31 15:10:15 +0200

    Added particle and sound effects for when elytra dont work

--- changelog.md
+++ changelog.md
@@ -2,6 +2,7 @@
 
 - Added 'Scared' effect whenever a player is targeted by an enderman
 - Changed player death handling. Players will now be forced into spectator mode for 30 minutes instead of being temporarily banned
+- Improved some parts of the code
 - Fixed bug where players would recieve the 'Cloud' effect when joining the server
 - Fixed spawning of magma slimes. There now is a maximum amount of magma slimes that can spawn near the player
 - Removed weakness effect upon regeneration
--- src/com/ExpertMode/Modules/ElytraBlocker.java
+++ src/com/ExpertMode/Modules/ElytraBlocker.java
@@ -1,12 +1,20 @@
 package com.ExpertMode.Modules;
 
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
 
 import com.ExpertMode.Main;
 import com.ExpertMode.Module;
+import com.PluginBase.LocationHelper;
+import com.PluginBase.ParticleEmitter;
+import com.PluginBase.SoundEmitter;
 import com.PluginBase.TimeHelper;
 
 public class ElytraBlocker extends Module {
@@ -30,6 +38,50 @@ public class ElytraBlocker extends Module {
 			
 			// Stop the player from gliding
 			player.setGliding(false);
+			
+			// Get the location where the particles should spawn
+			Location particleLocation =
+					LocationHelper.getInstance().offsetLocation
+					(
+							player.getLocation(),					// player location
+							new Vector								// location slightly in front of the player
+							(
+									player.getVelocity().getX() * 20,
+									0,
+									player.getVelocity().getZ() * 20
+							)
+					);
+			
+			// Emit some particles to let the player know he's not lagging
+			ParticleEmitter.getInstance().emitParticles
+			(
+					world,											// world
+					particleLocation,								// location
+					Particle.SWEEP_ATTACK,							// particle
+					1,												// amount
+					1,												// speed
+					new Vector(0, 0, 0)								// spread
+			);
+			ParticleEmitter.getInstance().emitParticles
+			(
+					world,
+					particleLocation,
+					Particle.CLOUD,
+					5,
+					0.1,
+					new Vector(0.25, 0.25, 0.25)
+			);
+			
+			// Player some sound at the players location
+			SoundEmitter.getInstance().emitSound
+			(
+					world,											// world
+					player.getLocation(),							// location
+					Sound.BLOCK_BEACON_DEACTIVATE,					// sound
+					SoundCategory.BLOCKS,							// sound category
+					1,												// volume
+					1												// pitch
+			);
 		}
 	}
 }
